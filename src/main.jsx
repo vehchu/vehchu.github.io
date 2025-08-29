ae68d1a2aad5eda065319e017b019bf1cf159460
MaryModest'/0/ <20800073@usc.edu.ph>
2025-08-29 14:03:41 +0800

    Add: Initial landing page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
 import "./index.css";
+import LandingPage from "./pages/LandingPage";
 
-createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <MantineProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+  </StrictMode>
+);
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import {
+  Container,
+  Title,
+  Text,
+  Button,
+  Badge,
+  Group,
+  Card,
+  Image,
+  SimpleGrid,
+  Stack,
+} from "@mantine/core";
+
+export default function LandingPage() {
+  return (
+    <main>
+      <Container>
+        {/* VI Image thingy + Word */}
+        {/*  */}
+        <h1>Tester</h1>
+      </Container>
+    </main>
+  );
+}
